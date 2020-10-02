9bf77b64b27e25f18af996ef3dad612e2a705ae3
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-10-02 17:20:36 -0600

    rename

--- tests/_config/_users.js
+++
@@ -1,4 +0,0 @@
-export const USERS = {
-  standard: { email: "test@test.com", sub: "1234345", permissions: [], roles: [] },
-  admin: { email: "test@test.com", sub: "1234345", permissions: [], roles: ["admin"] },
-}
