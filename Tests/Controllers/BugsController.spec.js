c91d19552a40feaf206c2d5af88813830db07604
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-10-02 16:59:47 -0600

    bad notes

--- Tests/Controllers/BugsController.spec.js
+++ Tests/Controllers/BugsController.spec.js
@@ -30,8 +30,6 @@ ava.serial("Must be logged in to Get Bugs", async (t) => {
   }
 })
 
-
-//Some comment somewhere
 ava.serial("Logged in user Can Get Bugs", async (t) => {
   try {
     authMock.setMockUserInfo(USERS.standard)
