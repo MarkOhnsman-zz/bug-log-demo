48c06d2fe8842f61761bfd580882a8bf61f5f603
Jake Overall <joverall22@gmail.com>
2020-10-03 01:28:56 -0600

    remove double run for pretest

--- __tests__/_config/_mockDb.js
+++ __tests__/_config/_mockDb.js
@@ -2,16 +2,22 @@ import { MockMongoose } from 'mock-mongoose';
 import mongoose from "mongoose";
 import DbConnection from '../../server/db/DbConfig';
 
-const mockMongoose = new MockMongoose(mongoose);
+let done = false
+async function buildStorage() {
+  if (done) { return console.log("[STORAGE READY]") }
+  const mockMongoose = new MockMongoose(mongoose);
+  await mockMongoose.prepareStorage()
+  await DbConnection.connect('mongodb://test/db');
+  done = true
+}
 
 export async function EstablishFakeDb() {
   try {
     console.log("Connecting to DB")
     let i = setInterval(() => {
-      console.log('\x1b[31m','connecting....')
+      console.log('\x1b[31m', '  connecting....')
     }, 300)
-    await mockMongoose.prepareStorage()
-    await DbConnection.connect('mongodb://test/db');
+    await buildStorage()
     clearInterval(i)
   } catch (e) {
     console.error(e)
